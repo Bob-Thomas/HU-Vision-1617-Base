80916a7d33b1d5118b93787848dfb9a2e29d1fc0
woodage <r-valkenburg@hotmail.com>
2017-04-12 19:56:20 +0200

    some comments in code and pdf of newest 'meetrapport enquete'

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -4,22 +4,23 @@ IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &imag
 	GrayscaleAlgorithm grayScaleAlgorithm;
 	IntensityImageStudent *test = new IntensityImageStudent(image.getWidth(), image.getHeight());
 	IntensityImage * test1 = new IntensityImagePrivate(image.getWidth(), image.getHeight());
-
+	// get the current time
 	auto t1 = std::chrono::high_resolution_clock::now();
+	// execute existing algoritm
 	grayScaleAlgorithm.doAlgorithm(image, *test1);
+	// get the current time again
 	auto t2 = std::chrono::high_resolution_clock::now();
+	// get the time of t2 and substract with t1. The remaining time is the execution time of the existing grayscale algoritm
 	auto speed1 = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
-
-
+	// get time again
 	auto t3 = std::chrono::high_resolution_clock::now();
+	// own implemented algoritm for grayscale
 	for (int i = 0; i < test->getWidth()*test->getHeight(); i++) {
 		// Intensity  algoritm
-		test->setPixel(i, 0.299 * (image.getPixel(i).r + image.getPixel(i).g + image.getPixel(i).b));
-
+		//test->setPixel(i, 0.299 * (image.getPixel(i).r + image.getPixel(i).g + image.getPixel(i).b));
 		// Luminace algoritm
-		//test->setPixel(i, ((float)image.getPixel(i).r*0.299) + ((float)image.getPixel(i).g * 0.587) + ((float)image.getPixel(i).b * 0.114));
-		
-
+		test->setPixel(i, ((float)image.getPixel(i).r*0.299) + ((float)image.getPixel(i).g * 0.587) + ((float)image.getPixel(i).b * 0.114));
+		// value algoritm
 		//test->setPixel(i, std::max({ image.getPixel(i).r, image.getPixel(i).g, image.getPixel(i).b}));
 	}
 	auto t4 = std::chrono::high_resolution_clock::now();
